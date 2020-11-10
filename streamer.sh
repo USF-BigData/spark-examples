19932c989b86f5d14dd7d4c648f1672f97675a41
Matthew Malensek <matt@malensek.net>
2020-11-09 23:31:28 -0800

    Add basic URI analysis and streaming example

---
+++ streamer.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Read each word in the UNIX spellcheck dictionary
+# and stream them out with a randomized delay to
+# simulate a real stream.
+#
+# NOTE: requires netcat (nc) and GNU sleep. The
+# sleep on Mac/BSD does not support fractional sleeps.
+
+for i in $(cat /usr/share/dict/words); do
+    echo $i
+    zzz=$(( $RANDOM % 3 ))
+    sleep 0.${zzz}
+done | nc -lk 9999
+
